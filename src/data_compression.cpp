94d19938f34b98a0301d602d7f3e18756a5931f2
Vladimir1t <vova.vexov@mail.ru>
2024-04-21 22:22:13 +0300

    update data_compression

--- src/data_compression.cpp
+++ src/data_compression.cpp
@@ -4,21 +4,27 @@ static FILE* error_file = fopen ("log\\file_error.txt", "w");
 
 static int compressed_data_output (FILE* result_file, unsigned char* buffer, size_t buffer_size, size_t string_size);
 
+static int get_data (FILE* sourse_file, char** str);
+
+static void print_library (struct Library* library);
+
 int main (int argc, char* argv[])
 {
     if (argc < 3)
         fprintf (error_file, "<< too few command arguments >>\n");
 
     struct Library library     = {0};
-    char           string[]    = "hello my friend how are you, i am fine, do you hear that great music, i love it very much, i have a dog jack and mom and father. english texts for beginners to practice reading and comprehension online and for free. practicing your comprehension of written english will both improve your vocabulary and understanding of grammar and word . order the texts below are designed to help you develop while giving you an instant evaluation of program.tqtweytweutqwuetyytvestuqqqqrbxerttttttttetrcvvvvvvvvvvvvvvvcutnxxxxxxxxxxxxxieoiuwyzebrnycywexrywuryerxrghjagdjajkahtyuqtetvxwtetqzeuqwtxqqwutbzewnqoimexirpociwuroqunxerubyxerqzgggiexuasjdkuirouwefeworiuroweirpmmqweqwxasweqzsqeaqxztiuworpowpoqmrcbxewryqzweppppplmkiuhucgytyfwtdfywelecmqxkefmoqircqupxmrigecnrjkwmdiweiowiofuihfreirfhwoijdowieifehwuifdmcncxzhfhsdjakhuyeraduihjhfjhasdkjfhjksadhfiawehjasdhfjkhavbnabbbaeiopdnjbdahdkljfnc";
-    //bjdshklfjskdlnuiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiicsbcjsddljaiosxxaqrqredsgaehuldjfklgotldkvnmxbncbzmxnmnczmjskljdalsjdoepaiodsgjdgiriogdhghjghjgjhgbnhiuuphfjdhfjkhsjkdhsjkhdjhfskjfowiueuyiuyriqowoiuk";
+    char*          string      = NULL;
     unsigned char* buffer      = NULL;
     size_t         buffer_size = 0;
 
     FOPEN (library_file, argv[1], "r");
-    FOPEN (result_file, argv[2], "wb");
+    FOPEN (sourse_file, argv[2], "r");
+    FOPEN (result_file, argv[3], "wb");
 
     read_library_file (library_file, &library);
+
+    get_data (sourse_file, &string);
 
     compress_data (string, &buffer, &buffer_size, &library);
 
@@ -34,6 +40,20 @@ int main (int argc, char* argv[])
     return 0;
 }
 
+int get_data (FILE* sourse_file, char** string)
+{
+    CHECK_PTR (sourse_file);
+    CHECK_PTR (string);
+
+    size_t file_size = file_size_measure (sourse_file);
+    //printf ("size of file: %d\n", file_size);
+
+    CALLOC (*string, char, file_size + 1);
+    size_t factual_size = fread (*string, sizeof (char), file_size, sourse_file);
+
+    return SUCCESS;
+}
+
 int read_library_file (FILE* library_file, struct Library* library)
 {
     CHECK_PTR (library_file);
@@ -43,8 +63,8 @@ int read_library_file (FILE* library_file, struct Library* library)
     library->size      = 0;
     CALLOC (library->lib_words, struct Library_word, library->capacity);
 
-    size_t file_size = file_size_measure (library_file);            // measures the size of a text
-    printf ("size of file: %d\n", file_size);
+    size_t file_size = file_size_measure (library_file);
+    //printf ("size of file: %d\n", file_size);
     char* text_data = NULL;
     CALLOC (text_data, char, file_size + 1);
     size_t factual_size = fread (text_data, sizeof (char), file_size, library_file);
@@ -55,28 +75,23 @@ int read_library_file (FILE* library_file, struct Library* library)
     char* word = strtok (text_data, sep);
     while (word != NULL)
     {
-        //printf ("lib size = %d\n", library->size);
         if (library->size == library->capacity)
         {
             library->capacity *= 2;
             REALLOC (library->lib_words, struct Library_word, library->capacity);
-            //library->lib_words = (struct Library_word*) realloc (library->lib_words, library->capacity * sizeof (char));
-            if (library->lib_words == NULL)
-                return ERROR;
         }
-        memcpy (library->lib_words[library->size].name, word, strlen (word) * sizeof (char));
-        //printf ("word = %s\n", library->lib_words[library->size].name);
-        //printf ("word = %s\n", word);
-        library->lib_words[library->size].type = one_byte;
-        library->lib_words[library->size].word_num.one_byte_num = library->size;
+        memcpy (library->lib_words[library->size].name, word, strlen (word));
+        library->lib_words[library->size].word_num = library->size;
         library->size += 1;
 
         word = strtok (NULL, sep);
     }
+    word = "\n";
+    memcpy (library->lib_words[library->size].name, word, strlen (word));
+    library->lib_words[library->size].word_num = library->size;
+    library->size += 1;
 
-    for (int i = 0; i < library->size; i++)
-        printf ("[%s]\n", library->lib_words[i].name);
-
+    print_library (library);
     free (text_data);
 
     return SUCCESS;
@@ -89,14 +104,14 @@ int compress_data (char* string, unsigned char** buffer, size_t* buffer_size, st
     CHECK_PTR (buffer_size);
     CHECK_PTR (library);
 
-    CALLOC (*buffer, unsigned char, 1000);
+    CALLOC (*buffer, unsigned char, 1000); //
 
     int ptr  = 0;
     int flag = 1;
-    int num  = 0xDEAD;
     int j = 0;
+    int num  = 0xDEAD;
 
-    while (string[ptr] != '\0')
+    while (string[ptr] != '\0')  //
     {
         num  = 0xDEAD;
         flag = 1;
@@ -110,63 +125,47 @@ int compress_data (char* string, unsigned char** buffer, size_t* buffer_size, st
         while (flag == 1 && string[ptr] != '\0')
         {
             word[j++] = string[ptr];
-            //printf ("[[%s]]\n", word_1);
-            //j++;
+
             for (int i = 0; i < library->size; i++)
             {
                 flag = 0;
                 if (!strcmp (word, library->lib_words[i].name))
                 {
-                    printf ("<<%s>>\n", word);
-                    //ptr++;
+                    //printf ("<<%s>>\n", word);
                     new_library_word[j] = string[++ptr];
                     flag = 1;
-                    //if (library->lib_words[i].type == one_word)
-                    if (library->lib_words[i].type == one_byte)
-                        num = library->lib_words[i].word_num.one_byte_num;
-                    else
-                        num = library->lib_words[i].word_num.two_byte_num;
+                    num = library->lib_words[i].word_num;
                     break;
                 }
             }
         }
-        if (library->size == library->capacity)         // make library bigger
+        if (library->size == library->capacity)         // make capaity of the library bigger
         {
             library->capacity *= 2;
             REALLOC (library->lib_words, struct Library_word, library->capacity);
         }
-        memcpy (library->lib_words[library->size].name, new_library_word, strlen (new_library_word) * sizeof (char));   // add new word in library
-        if (library->size < MAX_ONE_BYTE_WORDS_NUM)
-        {
-            library->lib_words[library->size].word_num.one_byte_num = library->size;
-            library->lib_words[library->size].type = one_byte;
-        }
-        else
-        {
-            library->lib_words[library->size].word_num.two_byte_num = library->size;
-            library->lib_words[library->size].type = two_byte;
-        }
+        memcpy (library->lib_words[library->size].name, new_library_word, strlen (new_library_word));   // add new word in library
+        library->lib_words[library->size].word_num= library->size;
         library->size += 1;
 
-        if (num < MAX_ONE_BYTE_WORDS_NUM)   // add one_byte_word in library
+        if (num < MAX_ONE_BYTE_WORDS_NUM)   // add one_byte_word in library (max = 128 words)
         {
             (*(buffer))[*buffer_size] = (unsigned char) num;
-            printf ("num = %u (one byte code)\n", (*buffer)[*buffer_size]);
+            //printf ("num = %u (one byte code)\n", (*buffer)[*buffer_size]);
             *buffer_size += 1;
         }
-        else if (num < MAX_LIBRARY_SIZE)    // add two_byte_word in library // до 32...
+        else if (num < MAX_LIBRARY_SIZE)    // add two_byte_word in library (max = 32767)
         {
-            unsigned short mask = 128;
-            unsigned short add = 32896;
-            unsigned short zero = 0;
-            printf ("num = %u (two byte code)\n",  (num & mask));
-            unsigned short res = num & mask;
-            if (res == zero)
-                num = num | add;
-            //num += 128;
+            unsigned short mask     = 128;
+            unsigned short add_mask = 32896;
+            unsigned short zero     = 0;
+            //printf ("num = %u (two byte code)\n", (num & mask));
+            //unsigned short res = num & mask;
+            if ((num & mask) == zero)
+                num = num | add_mask;
+
             memcpy (*buffer + *buffer_size, &num, sizeof (unsigned short));
-            printf ("num = %u (two byte code)\n",  *((unsigned short*) (*buffer + *buffer_size)));
-            printf ("%u and %u\n",  (*(buffer))[*buffer_size],  (*(buffer))[*buffer_size + 1]);
+
             *buffer_size += 2;
         }
     }
@@ -191,11 +190,20 @@ int compressed_data_output (FILE* result_file, unsigned char* buffer, size_t buf
     CHECK_PTR (result_file);
     CHECK_PTR (buffer);
 
-    //printf ("size of library = %d\n", library->size);
     printf ("Before %d bytes\n"
             "After  %d bytes\n", string_size, buffer_size);
 
     fwrite (buffer, sizeof(unsigned char), buffer_size, result_file);
-    //fprintf (result_file, "$$");
     return SUCCESS;
+}
+
+void print_library (struct Library* library)
+{
+    printf ("----------------- Library -----------------\n\n");
+    for (int i = 0; i < library->size; i++)
+        printf ("[%s]", library->lib_words[i].name);
+
+    printf ("\n");
+    printf ("size of library = %d\n\n", library->size);
+    printf ("-------------------------------------------\n");
 }
