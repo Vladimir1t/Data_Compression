f1ae26942e6e96052e2ba7ad6a7dc5209e9e36c5
Vladimir1t <vova.vexov@mail.ru>
2024-04-28 12:58:26 +0300

    add new defines

--- include/library_of_defines.h
+++ include/library_of_defines.h
@@ -36,4 +36,65 @@
         return OPEN_ERROR;                                   \
     }
 
+#define INCREASE_CAPACITY(elem)                                                                         \
+    {                                                                                                   \
+        library->capacity *= 2;                                                                         \
+        REALLOC (library->lib_words, struct Library_word, library->capacity);                           \
+    }
+
+#define INCREASE_BUFFER_CAPACITY(elem)                                                                   \
+    {                                                                                                    \
+        (buffer->capacity) *= 2;                                                                         \
+        REALLOC (buffer->data, unsigned char, buffer->capacity);                                         \
+    }
+
+#define ADD_IN_LIBRARY(new_library_word)                                                                    \
+    {                                                                                                       \
+        memcpy (library->lib_words[library->size].name, new_library_word, strlen (new_library_word));       \
+        library->lib_words[library->size].word_num = library->size;                                         \
+        library->size += 1;                                                                                 \
+    }
+
+#define ENCODE_WORD_AS_ONE_BYTE(num)                                                  \
+    {                                                                                 \
+        (buffer->data)[buffer->size] = (unsigned char) num;                           \
+        buffer->size += 1;                                                            \
+    }                                                                                 \
+
+#define ENCODE_WORD_AS_TWO_BYTES(num)                                                 \
+    {                                                                                 \
+        unsigned short mask     = 128;        /*0000 0000 1000 0000*/                 \
+        unsigned short add_mask = 32896;      /*1000 0000 1000 0000*/                 \
+        unsigned short zero     = 0;                                                  \
+        if ((num & mask) == zero)                                                     \
+            num = num | add_mask;                                                     \
+        memcpy (buffer->data + buffer->size, &num, sizeof (unsigned short));          \
+        buffer->size += 2;                                                            \
+    }
+
+#define DECODE_WORD(num, ...)                                       \
+    if ((text_data[ptr] & MASK) == ONE_BYTE_WORD)                   \
+        num = text_data[ptr++];                                     \
+    else                                                            \
+    {                                                               \
+        unsigned short mask = 32896;     /*1000 0000 1000 0000*/    \
+        num = *((unsigned short*) (text_data + ptr));               \
+        unsigned short res = num & mask;                            \
+        if (res == mask)                                            \
+            num = num ^ mask;                                       \
+        ptr += 2;                                                   \
+    }
+
+#define DECODE_NEXT_WORD(num, ...)                                  \
+    if ((text_data[ptr] & MASK) == ONE_BYTE_WORD)                   \
+        num = text_data[ptr];                                       \
+    else                                                            \
+    {                                                               \
+        unsigned short mask = 32896;     /*1000 0000 1000 0000*/    \
+        num = *((unsigned short*) (text_data + ptr));               \
+        unsigned short res = num & mask;                            \
+        if (res == mask)                                            \
+            num = num ^ mask;                                       \
+    }
+
 #endif // LIBRARY_OF_DEFINES_H_INCLUDED
